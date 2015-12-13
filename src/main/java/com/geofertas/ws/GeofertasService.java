26481a65b30584d0e9e23c2c554eab568b63e536
miguetv17 <miguetv17@gmail.com>
2015-12-12 21:20:38 -0500

    update services interface

--- src/main/java/com/geofertas/ws/GeofertasService.java
+++ src/main/java/com/geofertas/ws/GeofertasService.java
@@ -1,7 +1,11 @@
 package com.geofertas.ws;
 
+import com.geofertas.entities.Advertisement;
+import com.geofertas.entities.User;
+
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import java.util.List;
 
 /**
  * Created by whoami on 11/22/15.
@@ -13,6 +17,14 @@ interface GeofertasService {
     // Customizes the mapping of the return value to a WSDL part and XML element.s
     @WebResult
     String sayHello(String name);
+
+    User authenticate(String username, String hashPassword);
+    List<Advertisement> getAdvertisements(Double lat, Double lon);
+    Boolean saveAdvertisement(Integer advertisementID, Integer userId);
+    List<Advertisement> getSavedAdvertisements(Integer userID);
+    User updateUser(User user);
+    User registerUser(User user);
+    Boolean resetPasword(Integer userID);
 
 
 
